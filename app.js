475c13558be2ab8b08007bbc03eea2be90f305dc
youjin kim <gracecancho@gmail.com>
2023-10-23 14:01:28 +0900

    함수 타입 및 콜백

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Time to get started...");
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log("Result " + num);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
