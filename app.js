72180e030c4a4d06e8ff5f320a1b6d22642f83e3
youjin kim <gracecancho@gmail.com>
2023-10-23 14:27:51 +0900

    unknown

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result " + num);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
