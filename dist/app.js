b1337cc207f292cf5897cb06c3826c9010674c7a
youjin kim <gracecancho@gmail.com>
2023-10-24 10:16:26 +0900

    arrow function

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,12 @@
 "use strict";
+const userName = 'Max';
+let age = 30;
+age = 29;
+const add = (a, b) => a + b;
+const printOutput = output => console.log(output);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+printOutput(add(5, 2));
 //# sourceMappingURL=app.js.map
