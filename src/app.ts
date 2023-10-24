2d07fbb7d424b8cec1e69090e9e3fb51556114cc
youjin kim <gracecancho@gmail.com>
2023-10-24 14:23:22 +0900

    구별 된 유니언

--- src/app.ts
+++ src/app.ts
@@ -70,10 +70,33 @@ function useVehicle(vehicle: Vehicle) {
   if (vehicle instanceof Truck) {
     vehicle.loadCargo(1000);
   }
-  if ("loadCargo" in vehicle) {
-    vehicle.loadCargo(1000);
-  }
 }
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse; // union type
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
