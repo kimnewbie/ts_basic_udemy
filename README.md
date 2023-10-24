ea0b92a12e1c1ac9b15db5a643c14c76d529afa5
youjin kim <gracecancho@gmail.com>
2023-10-24 09:55:43 +0900

    init

--- README.md
+++
@@ -1,3 +0,0 @@
-https://www.udemy.com/course/best-typescript-21/
-
-바탕으로 실습한 내용
--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,2 @@
 "use strict";
-const button = document.querySelector("button");
-function clickHandler(message) {
-    let userName = "Max";
-    console.log("Clicked! " + message);
-}
-if (button) {
-    button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
-}
+//# sourceMappingURL=app.js.map
